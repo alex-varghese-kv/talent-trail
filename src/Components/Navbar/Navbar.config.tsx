262c8d7f959f913416ba31efc71fda397e6adb29
Alex Varghese <alex.v@keyvalue.systems>
2023-09-15 14:29:58 +0530

    navigation updated

--- src/Components/Navbar/Navbar.config.tsx
+++ src/Components/Navbar/Navbar.config.tsx
@@ -1,24 +1,24 @@
-import { pagePaths } from 'config/pages';
+import { pagePaths } from "config/pages";
 
 export const navbarItems = [
   {
-    title: 'My jobs',
-    path: pagePaths.createJob,
-    icon: '/media/icon_3.1dfe15d1.svg',
+    title: "My jobs",
+    path: pagePaths.myJobs,
+    icon: "/media/icon_3.1dfe15d1.svg",
   },
   {
-    title: 'Submit job',
+    title: "Submit job",
     path: pagePaths.createJob,
-    icon: '/media/icon_39_active.084f5967.svg',
+    icon: "/media/icon_39.e0d1c743.svg",
   },
   {
-    title: 'Saved candidate',
+    title: "Saved candidate",
     path: pagePaths.createJob,
-    icon: '/media/icon_6.70ec5244.svg',
+    icon: "/media/icon_6.70ec5244.svg",
   },
   {
-    title: 'Account settings',
+    title: "Account settings",
     path: pagePaths.createJob,
-    icon: '/media/icon_7.9db14e45.svg',
+    icon: "/media/icon_7.9db14e45.svg",
   },
 ];
